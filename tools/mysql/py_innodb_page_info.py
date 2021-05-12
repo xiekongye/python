5037605104adfbae493b6b14aa98c17dcd71c872
lieying <lieying@pinduoduo.com>
2021-05-12 14:19:34 +0800

    命令行用法

--- tools/mysql/py_innodb_page_info.py
+++ tools/mysql/py_innodb_page_info.py
@@ -5,6 +5,7 @@ from sys import argv
 from mylib import myargv
 
 if __name__ == '__main__':
+	# myargv = myargv(argv) #命令行用法
 	myargv = myargv(['-v','user.ibd'])
 	if myargv.parse_cmdline() == 0:
 		pass
